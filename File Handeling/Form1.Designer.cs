36e30acacb6b5d8fe89e79b29630e9365bfd2ae7
wasif888 <95183072+wasif888@users.noreply.github.com>
2022-03-22 01:56:07 +0600

    Add files via upload

---
+++ File Handeling/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace FileHandling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EmployeeID = new System.Windows.Forms.ListBox();
+            this.NameListBox = new System.Windows.Forms.ListBox();
+            this.SearchIdBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.salaryLabel = new System.Windows.Forms.Label();
+            this.hireDateLabel = new System.Windows.Forms.Label();
+            this.IDLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EmployeeID
+            // 
+            this.EmployeeID.FormattingEnabled = true;
+            this.EmployeeID.ItemHeight = 16;
+            this.EmployeeID.Location = new System.Drawing.Point(49, 124);
+            this.EmployeeID.Name = "EmployeeID";
+            this.EmployeeID.Size = new System.Drawing.Size(120, 148);
+            this.EmployeeID.TabIndex = 0;
+            // 
+            // NameListBox
+            // 
+            this.NameListBox.FormattingEnabled = true;
+            this.NameListBox.ItemHeight = 16;
+            this.NameListBox.Location = new System.Drawing.Point(165, 124);
+            this.NameListBox.Name = "NameListBox";
+            this.NameListBox.Size = new System.Drawing.Size(120, 148);
+            this.NameListBox.TabIndex = 1;
+            // 
+            // SearchIdBox
+            // 
+            this.SearchIdBox.Location = new System.Drawing.Point(489, 38);
+            this.SearchIdBox.Name = "SearchIdBox";
+            this.SearchIdBox.Size = new System.Drawing.Size(100, 22);
+            this.SearchIdBox.TabIndex = 2;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(499, 80);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Search ID";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(440, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(440, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(440, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hire Date:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(440, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Salary:";
+            // 
+            // salaryLabel
+            // 
+            this.salaryLabel.AutoSize = true;
+            this.salaryLabel.Location = new System.Drawing.Point(555, 267);
+            this.salaryLabel.Name = "salaryLabel";
+            this.salaryLabel.Size = new System.Drawing.Size(0, 16);
+            this.salaryLabel.TabIndex = 11;
+            // 
+            // hireDateLabel
+            // 
+            this.hireDateLabel.AutoSize = true;
+            this.hireDateLabel.Location = new System.Drawing.Point(555, 228);
+            this.hireDateLabel.Name = "hireDateLabel";
+            this.hireDateLabel.Size = new System.Drawing.Size(0, 16);
+            this.hireDateLabel.TabIndex = 10;
+            // 
+            // IDLabel
+            // 
+            this.IDLabel.AutoSize = true;
+            this.IDLabel.Location = new System.Drawing.Point(555, 192);
+            this.IDLabel.Name = "IDLabel";
+            this.IDLabel.Size = new System.Drawing.Size(0, 16);
+            this.IDLabel.TabIndex = 9;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(555, 158);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(0, 16);
+            this.nameLabel.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.salaryLabel);
+            this.Controls.Add(this.hireDateLabel);
+            this.Controls.Add(this.IDLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.SearchIdBox);
+            this.Controls.Add(this.NameListBox);
+            this.Controls.Add(this.EmployeeID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox EmployeeID;
+        private System.Windows.Forms.ListBox NameListBox;
+        private System.Windows.Forms.TextBox SearchIdBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label salaryLabel;
+        private System.Windows.Forms.Label hireDateLabel;
+        private System.Windows.Forms.Label IDLabel;
+        private System.Windows.Forms.Label nameLabel;
+    }
+}
+
---
+++ File Handeling/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileHandling
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+           
+             DateTime clickTime = DateTime.Now;
+            string ID = SearchIdBox.Text;
+
+                for (int i = 0; i < empList.Count; i++)
+                {
+                    if (empList[i].ID == ID)
+                    {
+                        nameLabel.Text = empList[i].Name;
+                        IDLabel.Text = empList[i].ID;
+                        hireDateLabel.Text = empList[i].HireDate;
+                        salaryLabel.Text = empList[i].Salary;
+
+                    string path = @"C:\Users\ASUS\Desktop\OOC lab\FileHandling\File.txt";
+             
+                            File.AppendAllText(path,empList[i].ID+" "+empList[i].Name+" "+empList[i].HireDate+" "+empList[i].Salary+" " + clickTime +"\n");
+     
+                    }
+                }
+                if(IDLabel.Text != ID)
+            {
+                MessageBox.Show("Employee not found!");
+            }
+            
+        }
+
+        List<Employee> empList = new List<Employee>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var reader = new StreamReader(@"C:\Users\ASUS\Desktop\OOC lab\FileHandling\employees.csv"))
+            {
+                
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    Employee emp = new Employee();
+
+                    emp.ID = values[0];
+                    emp.Name = values[1];
+                    emp.HireDate = values[5];
+                    emp.Salary = values[7];
+
+                    empList.Add(emp);
+                }
+
+                for(int i = 0; i < empList.Count; i++)
+                {
+                    EmployeeID.Items.Add(empList[i].ID);
+                    NameListBox.Items.Add(empList[i].Name);
+                }
+            }
+        }
+    }
+}
